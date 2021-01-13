f67e76af56c0163a6086728d23029467da5f2c66
Kailash Chandra Biswal <64208998+kailashbiswal1992@users.noreply.github.com>
2021-01-13 12:28:06 +0530

    My Project Commit 2

--- newCode.java
+++ newCode.java
@@ -6,22 +6,6 @@ public class newCode {
 		
 		
 		System.out.println("Exxat System");
-		//Automation for new application//
-		
-	//	 System.setProperty("webdriver.chrome.driver","C:\\Users\\kailashchandra.biswa\\Downloads\\chromedriver_win32\\chromedriver.exe");  
-		
-	//	WebDriver driver = new ChromeDriver();
-		
-		////driver.manage().window().maximize();
-		
-		//driver.navigate().to("http://qaintegration.westindia.cloudapp.azure.com:5200/dashboard");
-		
-		
-		//driver.findElement(By.name("userName")).sendKeys("exxat.superadmin");
-	//	driver.findElement(By.name("password")).sendKeys("admin");
-		
-		
-		
 		
 		
 	}
